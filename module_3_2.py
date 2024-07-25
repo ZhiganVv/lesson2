9bf74966735be2de75ac40ca5d88c9d4aa1c347f
ZhiganVv <zhiganov.vladimir.95@gmail.com>
2024-07-25 17:27:59 +0700

    module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -17,22 +17,17 @@
 
 
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    while True:
-        if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
-            if "@" not in recipient and sender:
-                break
-        else:
+    if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
+        if '@' not in recipient and sender:
             print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-            break
-        if sender == recipient:
+        elif sender == recipient:
             print(f'Нельзя отправить письмо самому себе!')
-            break
         elif sender == "university.help@gmail.com":
             print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-            break
-        elif sender != "university.help@gmail.com":
+        else:
             print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
-            break
+    else:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
